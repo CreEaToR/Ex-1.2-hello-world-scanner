51ac18fa00a562b95fddd90e21b81d58a65ca5c3
CreEaToR <rockko10@gmail.com>
2015-09-09 15:11:41 -0400

    Exercise 1.2 hello world program.

---
+++ A1/src/Excerise1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Excerise1 {
+	
+	public static void main(String args[]){
+		
+		Scanner in = new Scanner(System.in);
+	
+	System.out.println("Enter your username");
+   	
+	String name = in.next();
+	
+	System.out.println("Enter your password");
+	
+	String password = in.next();
+	
+	System.out.println("Hello " + name + ", Welcome to CSC200 class! and Your password is " + password);
+	
+	
+	}
+	
+
+}
